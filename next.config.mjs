5335211506d35407cc292496c82c6f1cbcc236f4
Siddhanta Chandra <iamsiddhanta.10@gmail.com>
2025-05-24 07:46:09 +0530

    Changed config for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['127.0.0.1', 'unbemrwqevcofbsxrohg.supabase.co'],
+    domains: ['bcslaoindjadawhkdrhk.supabase.co'],
   },
   eslint: {
     // Disable ESLint during builds to avoid blocking on style issues
